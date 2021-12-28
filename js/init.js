efd40911dc248e7e59104553299a2d73d9a669ea
Code Sandbox <hello@codesandbox.io>
2021-12-28 05:14:08 +0000

    Deployment from Codesandbox.

---
+++ js/init.js
@@ -0,0 +1,7 @@
+(function ($) {
+  $(function () {
+    $(".sidenav").sidenav();
+    $(".parallax").parallax();
+    $(".fixed-action-btn").floatingActionButton();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
